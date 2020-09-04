2a3f5af9d2c26cd5956f9f7e156b26770a30d0e6
Michael Nagle <michael.nagle@oregonstate.edu>
2020-09-04 15:21:29 -0700

    Update x_import_utils.py

--- x_import_utils.py
+++ x_import_utils.py
@@ -1,4 +1,4 @@
-def read_fit_spectra(wavelengths, plot=False, spectra_noise_threshold = 0.01):
+def read_fit_spectra(spectra_path, wavelengths, plot=False, spectra_noise_threshold = 0.01):
     
     numpy2ri.activate()
 
