969698303b5855207dd6c0ba9f28d05c146df485
Michael Nagle <michael.nagle@oregonstate.edu>
2020-09-17 20:30:27 -0700

    fix indent that broke build

--- gmodetector_py/misc_utils.py
+++ gmodetector_py/misc_utils.py
@@ -1,29 +1,29 @@
 def read_wavelengths(file_path):
-    """ Obtain a list of wavelengths for which hyperspectral data has been collected and stored in the input header file 
-    
+    """ Obtain a list of wavelengths for which hyperspectral data has been collected and stored in the input header file
+
     :param file_path: A string indicating the path to an ENVI header (.hdr) file which contains wavelength information. This should be the same for all images taken with a given set of hyperspectral camera settings (specifically, the same wavelengths collected).
     ...
     return: A list of precise wavelengths
-    
-    
+
+
     """
     import spectral.io.envi as envi
     h = envi.read_envi_header(file_path)
     wavelengths = h['wavelength']
     return(wavelengths)
-    
+
 def find_desired_indices(wavelengths, min_desired_wavelength, max_desired_wavelength):
     """ Determine indices, in a wavelength list, for which wavelengths are within the desired range specified by the user
-    
+
     :param wavelengths: A list of precise wavelengths, for which indices of interest will be determined.
     :param min_desired_wavelength: A numeric value indicating a threshold BELOW which spectral data is excluded
     :param max_desired_wavelength: A numeric value indicating a threshold ABOVE which spectral data is excluded
     ...
     return: A list of indices corresponding to desired wavelengths
-    
+
     >>> find_desired_indices([1, 2, 3, 4, 5], 1.5, 4.5)
     [2, 3, 4]
-    
+
     """
     import numpy as np
     wavelengths = np.asarray(wavelengths)
