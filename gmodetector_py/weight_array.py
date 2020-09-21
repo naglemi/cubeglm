0cda0b5b7bee3cb6a825c1cba8ae3286032cd556
Michael Nagle <michael.nagle@oregonstate.edu>
2020-09-21 16:15:30 -0700

    Update weight_array.py

--- gmodetector_py/weight_array.py
+++ gmodetector_py/weight_array.py
@@ -21,10 +21,10 @@ class WeightArray:
             if i == 0:
                 # Thank you 英文原文 for explaining conversion of matrix to triplet form with numpy http://www.javaear.com/question/30478758.html
                 I, J = np.indices(self.weights.shape[0:2])
-                array_in_coordinate = pd.DataFrame(np.column_stack(ar.ravel() for ar in (I, J, self.weights[:, :, i])),
+                array_in_coordinate = pd.DataFrame(np.column_stack(ar.ravel().tolist() for ar in (I, J, self.weights[:, :, i])),
                 columns = ['rows', 'cols', self.components[i]])
             if i > 0:
-                matrix_slice_in_triplet = np.column_stack(ar.ravel() for ar in (I, J, self.weights[:, :, i]))
+                matrix_slice_in_triplet = np.column_stack(ar.ravel().tolist() for ar in (I, J, self.weights[:, :, i]))
                 array_in_coordinate = pd.concat([array_in_coordinate,
                 pd.DataFrame(matrix_slice_in_triplet[:, 2], columns = [self.components[i]])], axis = 1)
                 print('array shape is...')
@@ -33,7 +33,7 @@ class WeightArray:
                 print(array_in_coordinate[:1])
         array_in_coordinate = pd.DataFrame(array_in_coordinate)
         # columns = ['rows', 'cols'] + self.components)
-        array_in_coordinate.to_csv(path + str('_weights.csv'))
+        array_in_coordinate.to_csv(path + str('_weights.csv'), index = False)
 
     def plot(self, desired_component, color, cap):
         """Plot a single channel selected from a weight array produced by regression
