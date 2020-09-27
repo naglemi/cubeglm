80cbce5b3fc72545da06ea24ac072c10ff7d5604
Michael Nagle <michael.nagle@oregonstate.edu>
2020-09-27 01:20:46 -0700

    install, import h5py dependency

--- gmodetector_py/hypercube.py
+++ gmodetector_py/hypercube.py
@@ -2,6 +2,7 @@ import ntpath # Should work on all platforms for finding basename from file path
 import os
 import numpy as np
 import spectral as spy
+import h5py
 
 from gmodetector_py import read_wavelengths
 from gmodetector_py import find_desired_indices
