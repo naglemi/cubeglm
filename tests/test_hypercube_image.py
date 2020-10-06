026726aeb41ed8a26fb3b02f668088c2a3bb4b02
Michael Nagle <michael.nagle@oregonstate.edu>
2020-10-06 11:05:46 -0700

    format tests correctly for pytest

--- tests/test_hypercube_image.py
+++ tests/test_hypercube_image.py
@@ -10,31 +10,32 @@ from gmodetector_py import read_wavelengths
 # Need numpy for unit testing with allclose
 import numpy as np
 
-test_hypercube_path = "./tests/test_hypercube.hdr"
+def test_hypercube_image():
+    test_hypercube_path = "./tests/test_hypercube.hdr"
 
-wavelengths = read_wavelengths(file_path = test_hypercube_path)
+    wavelengths = read_wavelengths(file_path = test_hypercube_path)
 
-hypercube_de_novo = Hypercube(test_hypercube_path,
-                              min_desired_wavelength = 500,
-                              max_desired_wavelength = 900)
+    hypercube_de_novo = Hypercube(test_hypercube_path,
+                                  min_desired_wavelength = 500,
+                                  max_desired_wavelength = 900)
 
-hypercube_img_de_novo = FalseColor([ImageChannel(hypercube = hypercube_de_novo,
-                                                 desired_component_or_wavelength = '582.6952',
-                                                 color = 'green',
-                                                 cap = 1500),
-                                    ImageChannel(hypercube = hypercube_de_novo,
-                                                 desired_component_or_wavelength = '672.6443',
-                                                 color = 'red',
-                                                 cap = 800),
-                                    ImageChannel(hypercube = hypercube_de_novo,
-                                                 desired_component_or_wavelength = '501.2859',
-                                                 color = 'blue',
-                                                 cap = 650)])
+    hypercube_img_de_novo = FalseColor([ImageChannel(hypercube = hypercube_de_novo,
+                                                     desired_component_or_wavelength = '582.6952',
+                                                     color = 'green',
+                                                     cap = 1500),
+                                        ImageChannel(hypercube = hypercube_de_novo,
+                                                     desired_component_or_wavelength = '672.6443',
+                                                     color = 'red',
+                                                     cap = 800),
+                                        ImageChannel(hypercube = hypercube_de_novo,
+                                                     desired_component_or_wavelength = '501.2859',
+                                                     color = 'blue',
+                                                     cap = 650)])
 
-reloaded_test_hypercube_img = Image.open("./tests/test_hypercube.png")
+    reloaded_test_hypercube_img = Image.open("./tests/test_hypercube.png")
 
-# Need to convert images to numpy arrays so we can use np.allclose() for unit test
-unit_test_hypercube_img = np.array(hypercube_img_de_novo.image, dtype = 'uint8')
-reloaded_test_hypercube_img = np.array(reloaded_test_hypercube_img, dtype = 'uint8')
+    # Need to convert images to numpy arrays so we can use np.allclose() for unit test
+    unit_test_hypercube_img = np.array(hypercube_img_de_novo.image, dtype = 'uint8')
+    reloaded_test_hypercube_img = np.array(reloaded_test_hypercube_img, dtype = 'uint8')
 
-np.testing.assert_allclose(unit_test_hypercube_img, reloaded_test_hypercube_img)
+    np.testing.assert_allclose(unit_test_hypercube_img, reloaded_test_hypercube_img)
--- tests/test_weight_array_and_plots.py
+++ tests/test_weight_array_and_plots.py
@@ -17,60 +17,61 @@ import numpy as np
 
 from PIL import Image
 
-test_hypercube_path = "./tests/test_hypercube.hdr"
+def test_array_weights_plots():
+    test_hypercube_path = "./tests/test_hypercube.hdr"
 
-wavelengths = read_wavelengths(file_path = test_hypercube_path)
+    wavelengths = read_wavelengths(file_path = test_hypercube_path)
 
-min_desired_wavelength = 500
-max_desired_wavelength = 900
+    min_desired_wavelength = 500
+    max_desired_wavelength = 900
 
-hypercube_de_novo = Hypercube(test_hypercube_path,
-                              min_desired_wavelength = min_desired_wavelength,
-                              max_desired_wavelength = max_desired_wavelength)
+    hypercube_de_novo = Hypercube(test_hypercube_path,
+                                  min_desired_wavelength = min_desired_wavelength,
+                                  max_desired_wavelength = max_desired_wavelength)
 
-test_matrix = XMatrix(fluorophore_ID_vector = ['DsRed', 'ZsYellow', 'Chl', 'Diffraction'],
-                      spectral_library_path = "./spectral_library/",
-                      intercept = 1,
-                      wavelengths = wavelengths,
-                      spectra_noise_threshold = 0.01,
-                      min_desired_wavelength = min_desired_wavelength,
-                      max_desired_wavelength = max_desired_wavelength)
+    test_matrix = XMatrix(fluorophore_ID_vector = ['DsRed', 'ZsYellow', 'Chl', 'Diffraction'],
+                          spectral_library_path = "./spectral_library/",
+                          intercept = 1,
+                          wavelengths = wavelengths,
+                          spectra_noise_threshold = 0.01,
+                          min_desired_wavelength = min_desired_wavelength,
+                          max_desired_wavelength = max_desired_wavelength)
 
-weight_array_de_novo = WeightArray(test_matrix=test_matrix,
-                                   test_cube=hypercube_de_novo)
+    weight_array_de_novo = WeightArray(test_matrix=test_matrix,
+                                       test_cube=hypercube_de_novo)
 
-weight_array_de_novo.relu()
+    weight_array_de_novo.relu()
 
-reloaded_weight_array = pd.read_hdf('./tests/test_array_weights.hdf',
-                                    key = 'weights')
+    reloaded_weight_array = pd.read_hdf('./tests/test_array_weights.hdf',
+                                        key = 'weights')
 
-weight_array_de_novo._convert_3D_to_pseudotriplet()
+    weight_array_de_novo._convert_3D_to_pseudotriplet()
 
-print("Minimum value for both existing and de novo weight arrays for unit test:")
-print(weight_array_de_novo.weights.min())
-print(reloaded_weight_array.min())
+    print("Minimum value for both existing and de novo weight arrays for unit test:")
+    print(weight_array_de_novo.weights.min())
+    print(reloaded_weight_array.min())
 
-np.testing.assert_allclose(reloaded_weight_array,
-                           weight_array_de_novo._weights_pseudotriplet,
-                           equal_nan=False)
+    np.testing.assert_allclose(reloaded_weight_array,
+                               weight_array_de_novo._weights_pseudotriplet,
+                               equal_nan=False)
 
-reloaded_test_weights_img = Image.open("./tests/test_weights_FalseColor.png")
+    reloaded_test_weights_img = Image.open("./tests/test_weights_FalseColor.png")
 
-unit_test_weights_img = FalseColor([ImageChannel(weight_array = weight_array_de_novo,
-                                                 desired_component_or_wavelength = 'DsRed',
-                                                 color = 'green',
-                                                 cap = 400),
-                                    ImageChannel(weight_array = weight_array_de_novo,
-                                                 desired_component_or_wavelength = 'Chl',
-                                                 color = 'red',
-                                                 cap = 200),
-                                    ImageChannel(weight_array = weight_array_de_novo,
-                                                 desired_component_or_wavelength = 'Diffraction',
-                                                 color = 'blue',
-                                                 cap = 50)])
+    unit_test_weights_img = FalseColor([ImageChannel(weight_array = weight_array_de_novo,
+                                                     desired_component_or_wavelength = 'DsRed',
+                                                     color = 'green',
+                                                     cap = 400),
+                                        ImageChannel(weight_array = weight_array_de_novo,
+                                                     desired_component_or_wavelength = 'Chl',
+                                                     color = 'red',
+                                                     cap = 200),
+                                        ImageChannel(weight_array = weight_array_de_novo,
+                                                     desired_component_or_wavelength = 'Diffraction',
+                                                     color = 'blue',
+                                                     cap = 50)])
 
 
-unit_test_weights_img = np.array(unit_test_weights_img.image, dtype = 'uint8')
-reloaded_test_weights_img = np.array(reloaded_test_weights_img, dtype = 'uint8')
+    unit_test_weights_img = np.array(unit_test_weights_img.image, dtype = 'uint8')
+    reloaded_test_weights_img = np.array(reloaded_test_weights_img, dtype = 'uint8')
 
-np.testing.assert_allclose(unit_test_weights_img, reloaded_test_weights_img)
+    np.testing.assert_allclose(unit_test_weights_img, reloaded_test_weights_img)
